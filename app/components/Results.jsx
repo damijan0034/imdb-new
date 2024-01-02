ffd12d2c44a704401fafec430cd323a8bd892e29
Goran Kostic <damijan0034@gmail.com>
2024-01-02 10:20:36 +0100

    new

--- app/components/Results.jsx
+++ app/components/Results.jsx
@@ -4,7 +4,7 @@ import Card from './Card'
 const Results = ({results}) => {
   return (
     <div className='sm:grid sm:grid-cols-2 md:grid-cols-3
-                 lg:grid-cols-4 sm:p-10 md:px-20 '>
+                 lg:grid-cols-4 sm:p-10 p-3 md:px-20 '>
         {
             results.map((result)=>{
                 return(
